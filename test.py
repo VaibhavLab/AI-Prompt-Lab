e1927bfb0bcef7f8f0d9e670a069535448e8c3f8
noeticstech <realvaibhav2005@gmail.com>
2026-09-16 19:52:28 +0530

    continue

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-with open("test.txt", "r", encoding="utf-8") as file:
-    print(file.readline())
-    print(file.readline())
-    print(file.readline())
+def read_lines():
+    with open("test.txt" , "r" , encoding = "utf-8") as file:
+        for line in file:
+            yield line.strip()
+
+gen = read_lines()
+
+print(next(gen))
+print(next(gen))
+print(next(gen))
