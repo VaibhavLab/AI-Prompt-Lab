268a09f7658c48e982de245b38342f8915e44ac4
noeticstech <realvaibhav2005@gmail.com>
2026-09-16 19:16:36 +0530

    Phase 4 small mistake

--- storage.py
+++ storage.py
@@ -25,7 +25,7 @@ def save_data(prompts , responses):
     with open(temp_file, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4)
 
-    temp_file.replace("DATA_FILE")
+    temp_file.replace(DATA_FILE)
 
 def load_data():
     try:
